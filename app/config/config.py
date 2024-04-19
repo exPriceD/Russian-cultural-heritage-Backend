6bb3c74651d976742e857c5b9ec6e825e535affd
Maxim <lolerpro4@gmail.com>
2024-04-19 22:55:52 +0300

    [feat]: added config

---
+++ app/config/config.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    """Базовый конфигурационный класс."""
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-default-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///default.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    """Конфигурация для разработки."""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///dev.db'
+
+
+class TestingConfig(Config):
+    """Конфигурация для тестирования."""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///test.db'
+
+
+class ProductionConfig(Config):
+    """Конфигурация для продакшена."""
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///prod.db'
