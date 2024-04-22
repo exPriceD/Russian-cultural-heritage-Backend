8e4102910aeb62b83b86f7d9efaa6da3042c1895
Maxim <lolerpro4@gmail.com>
2024-04-22 18:51:53 +0300

    [improve]: changed logic of models

--- app/models.py
+++ app/models.py
@@ -6,21 +6,20 @@ class Facility(db.Model):
     __tablename__ = 'facility'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(65536), nullable=False)
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-class Detail(db.Model):
-    __tablename__ = 'detail'
-    id = db.Column(db.Integer, primary_key=True)
-    facility_id = db.Column(db.Integer, db.ForeignKey('facility.id'), nullable=False)
-    long_description = db.Column(db.Text, nullable=False)
-    model = db.Column(db.String(3000), nullable=True)
-
-
-class Image(db.Model):
+class Images(db.Model):
     __tablename__ = 'image'
     id = db.Column(db.Integer, primary_key=True)
     facility_id = db.Column(db.Integer, db.ForeignKey('facility.id'), nullable=False)
     image_type = db.Column(db.String(100), nullable=False)
     image_url = db.Column(db.String(300), nullable=False)
 
+
+class Models(db.Model):
+    __tablename__ = 'models'
+    id = db.Column(db.Integer, primary_key=True)
+    facility_id = db.Column(db.Integer, db.ForeignKey('facility.id'), nullable=False)
+    model_url = db.Column(db.String(300), nullable=False)
