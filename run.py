00847db1b792984f36c04dafe766c3af3c0ff78f
Maxim <lolerpro4@gmail.com>
2024-04-24 00:38:52 +0300

    [fix]: added blueprint registration

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app import create_app
-
-application = create_app()
+from app import application
+import app.views.views
+from app.views.views import api
 
 if __name__ == '__main__':
+    application.register_blueprint(api)
     application.run(load_dotenv=True)
