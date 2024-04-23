78673542dff420d4a2481979adcedf3420a8502c
Maxim <lolerpro4@gmail.com>
2024-04-24 01:32:24 +0300

    [feet]: Added the function of checking the file extension

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 import pytz
 
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp'}
+
 
 def get_current_time(timezone='Europe/Moscow'):
     moscow_tz = pytz.timezone(timezone)
     moscow_time = datetime.now(moscow_tz)
     formatted_time = moscow_time.strftime('%d.%m.%y %H:%M')
     return formatted_time
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
